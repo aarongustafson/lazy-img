66bf0c2e2c794ff6646271285f6e5f11d6efaf12
Aaron Gustafson <aaron@easy-designs.net>
2025-12-07 17:19:01 -0800

    Guard definition

--- define.js
+++ define.js
@@ -1,3 +1,18 @@
 import { LazyImgElement } from './lazy-img.js';
 
-customElements.define('lazy-img', LazyImgElement);
+export function defineLazyImg(tagName = 'lazy-img') {
+	const hasWindow = typeof window !== 'undefined';
+	const registry = hasWindow ? window.customElements : undefined;
+
+	if (!registry || typeof registry.define !== 'function') {
+		return false;
+	}
+
+	if (!registry.get(tagName)) {
+		registry.define(tagName, LazyImgElement);
+	}
+
+	return true;
+}
+
+defineLazyImg();
